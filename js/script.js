847dd84f59aea30fe96245eea3e80c8eb937c70f
Marzena <marzena.szopinska090@gmail.com>
2019-08-15 14:34:35 +0200

    working on new navigation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 const links = document.getElementById('nav-links');
 const navBar = document.querySelector('.navigation-bar');
 
-function toggleMenu() {
-  if(links.style.display === 'block'){
-    navBar.classList.remove('streatch');
-    links.style.display = 'none';
-  } else {
-    navBar.classList.add('streatch');
-    links.style.display = 'block';
-  }
-}
+// function toggleMenu() {
+//   if(links.style.display === 'block'){
+//     navBar.classList.remove('streatch');
+//     links.style.display = 'none';
+//   } else {
+//     navBar.classList.add('streatch');
+//     links.style.display = 'block';
+//   }
+// }
